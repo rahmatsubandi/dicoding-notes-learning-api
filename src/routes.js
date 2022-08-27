4638227ab5676ce053f29c187a6e4c714ef123fc
Rahmat Subandi <rhmtin12@gmail.com>
2022-08-27 13:41:46 +0700

    push code

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,6 @@
-const { addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler } = require('./handler');
+const {
+  addNoteHandler, getAllNotesHandler, getNoteByIdHandler, editNoteByIdHandler, deleteNoteByIdHandler,
+} = require('./handler');
 
 const routes = [
   {
@@ -6,21 +8,25 @@ const routes = [
     path: '/notes',
     handler: addNoteHandler,
   },
+
   {
     method: 'GET',
     path: '/notes',
     handler: getAllNotesHandler,
   },
+
   {
     method: 'GET',
     path: '/notes/{id}',
     handler: getNoteByIdHandler,
   },
+
   {
     method: 'PUT',
     path: '/notes/{id}',
     handler: editNoteByIdHandler,
   },
+
   {
     method: 'DELETE',
     path: '/notes/{id}',
